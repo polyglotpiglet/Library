c67ef0146a3dbe6ff94c1b46a57928a18c307aa2
polyglotpiglet <alexandra.p.batey@gmail.com>
2015-05-04 23:24:08 +0100

    typesafe config

--- src/main/scala/com/ojha/library/Main.scala
+++ src/main/scala/com/ojha/library/Main.scala
@@ -2,7 +2,12 @@ package com.ojha.library
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshalling.ToResponseMarshallable
+import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.PathMatchers.Segment
+import akka.http.scaladsl.server.Route
 import akka.stream.ActorFlowMaterializer
 import akka.stream.scaladsl.Sink
 import HttpMethods.GET
@@ -24,21 +29,40 @@ object Main extends App with Configurable {
 
   val serverSource = Http(system).bind(interface = host, port = port)
 
+  val baseUrl = config.getString("library.isbndb.url")
+  val key = config.getString("library.isbndb.key")
+  val tail = "/book/084930315X"
+
+  val url = baseUrl + key + tail
+
+
   val requestHandler: HttpRequest => HttpResponse = {
-    case HttpRequest(GET, Uri.Path("/"), _, _, _) =>
+//    case HttpRequest(GET, Uri.Path("/library/book"), _, _, _) =>
+    case HttpRequest(GET, Uri.Path("/library/book"), _, _ , _) =>
       HttpResponse(
-        entity = HttpEntity(MediaTypes.`text/html`,
-          "<html><body>Hello world!</body></html>"))
-
-    case HttpRequest(GET, Uri.Path("/ping"), _, _, _) =>
-      HttpResponse(entity = "PONG!")
-
-    case HttpRequest(GET, Uri.Path("/crash"), _, _, _) =>
-      sys.error("BOOM!")
+        entity = HttpEntity(MediaTypes.`text/plain`, url))
 
     case _: HttpRequest =>
       HttpResponse(404, entity = "Resource is not found!")
   }
+
+  val route: Route =
+  pathPrefix("library"){
+    path("book" / IntNumber) { id =>
+      get {
+        complete {
+          "Received GET request for order " + id
+        }
+      } ~
+        put {
+          complete {
+            "Received PUT request for order " + id
+          }
+        }
+    }
+  }
+
+
 
   val bindingFuture: Future[Http.ServerBinding] = serverSource.to(Sink.foreach { connection =>
     println("Accepted new connection from " + connection.remoteAddress)
