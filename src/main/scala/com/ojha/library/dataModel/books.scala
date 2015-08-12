e721e57813cf885563256c572e1cab6a92f76d24
polyglotpiglet <alexandra.p.batey@gmail.com>
2015-08-12 22:56:54 +0100

    improving api for library

--- src/main/scala/com/ojha/library/dataModel/books.scala
+++ src/main/scala/com/ojha/library/dataModel/books.scala
@@ -5,4 +5,4 @@ import com.ojha.library.book.BookApi.BookInfo
 /**
  * Created by alexandra on 05/05/15.
  */
-case class LibraryItem(bookInfo: BookInfo, category: String)
+
--- src/main/scala/com/ojha/library/library/Library.scala
+++ src/main/scala/com/ojha/library/library/Library.scala
@@ -1,14 +1,28 @@
 package com.ojha.library.library
 
-import com.ojha.library.dataModel.LibraryItem
+import com.ojha.library.book.BookApi.BookInfo
+import com.ojha.library.library.Library.LibraryItem
 
 /**
  * Created by alexandra on 05/05/15.
  */
+
+object Library {
+  case class LibraryItem(uniqueBookId: String, bookInfo: BookInfo, category: String)
+}
+
 trait Library {
 
   def addBook(item: LibraryItem)
 
-  def getAllCategories: List[String]
+  def getAllCategories: Set[String]
+
+  def getBookByAuthor(author: String): LibraryItem
+
+  def getBookByTitleSegment(titleText: String): LibraryItem
+
+  def getBooksByCategory(category: String): Set[LibraryItem]
+
+  def getAllBooks: Set[LibraryItem]
 
 }
